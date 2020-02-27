6a3f97398dfd7d9acba34b59f8aa3d6ee83c56a7
CryptoN999 <nicholasgallucci4@gmail.com>
2020-02-26 21:51:36 -0800

    working basic server w/ heroku deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const server = express();
+const LandingRouter = require("./server")
+
+server.use(express.json());
+
+const host = process.env.HOST || "http://127.0.0.1";
+const port = process.env.PORT || 4000;
+
+server.use("/", LandingRouter);
+
+server.listen(port, () => {
+    console.log(`\n*** Server is listening on ${host}:${port}`);
+})
