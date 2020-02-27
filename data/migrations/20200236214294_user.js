9265eb1e22b2702be9ccc19728effc1fe797480f
CryptoN999 <nicholasgallucci4@gmail.com>
2020-02-27 10:29:01 -0800

    finished migrations

--- data/migrations/20200236214294_user.js
+++ data/migrations/20200236214294_user.js
@@ -1,8 +1,38 @@
 exports.up = function(knex) {
     return knex.schema
-      .createTable('Items', tbl => {})
-      .createTable('Users', tbl => {})
-      ;
+      .createTable('Items', tbl => {
+        tbl.increments();
+        tbl.text('Owner')
+          .notNullable();
+        tbl.text('Title')
+          .notNullable();
+          tbl.text('Type')
+          .notNullable();
+        tbl.text('Description')
+          .notNullable();
+        tbl.text('Price');
+        .notNullable();
+        tbl.text('Availability');
+        .notNullable();
+        tbl.text('Brand');
+        tbl.text('Model');
+        tbl.text('imgURL');
+        tbl.text('Renter');
+      })
+      .createTable('Users', tbl => {
+        tbl.increments();
+        tbl.text('Password')
+          .notNullable();
+        tbl.text('Email')
+          .unique()
+          .notNullable();
+          tbl.text('FirstName')
+          .notNullable();
+        tbl.text('LastName')
+          .notNullable();
+        tbl.text('Phone');
+        tbl.text('Address');
+      })
   };
   
   exports.down = function(knex) {
