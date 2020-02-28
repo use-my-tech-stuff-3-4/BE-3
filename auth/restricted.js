394ae2c3bf047fa44930db5a6590790cdafe2c53
CryptoN999 <nicholasgallucci4@gmail.com>
2020-02-27 16:10:42 -0800

    cleaned up not needed code, auth fully tested/working

--- auth/restricted.js
+++ auth/restricted.js
@@ -1,18 +1,7 @@
-
 const jwt = require('jsonwebtoken');
 const {jwtSecret} = require('../config/secret');
 
-// module.exports = (req, res, next) => {
-//     const token = req.headers.authorization;
-//     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-//         if (err) {
-//             res.status(401).json({ message: "Invalid credentials" })
-//         } else {
-//             req.decodedToken = decodedToken;
-//             next();
-//         }
-//     });
-// };
+ 
 module.exports = (req, res, next) => {
     const token = req.headers.authorization;
     if(token) {
