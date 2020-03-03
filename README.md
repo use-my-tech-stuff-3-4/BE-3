006a4fe3ab74e11b1dc5c6b72b8ef0b8dfa7e058
CryptoN999 <nicholasgallucci4@gmail.com>
2020-03-02 20:45:39 -0800

    removed some tests

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BE-3
+https://documenter.getpostman.com/view/10085267/SzKYQxc2?version=latest / user register endpoint documentation
+
+
+https://documenter.getpostman.com/view/10085267/SzKZsFws?version=latest / items for rent endpoint documentation
--- helpers/items/itemsModel.spec.js
+++ helpers/items/itemsModel.spec.js
@@ -1,45 +1,49 @@
 const db = require('../../data/dbConfig');
 const users = require('../users/usersModel');
 
+
 describe('users model', () => {
     beforeEach(async () => {
         await db("users").truncate();
     });
+
     afterEach(async () => {
         await db("users").truncate();
     });
 
     const mockUser = {
-        username: "Nick", 
-        password: "password", 
-        email: "Nick@gmail.com", 
-        firstname: "Nick", 
-        lastname: "G", 
+        username: "Stan", 
+        password: "Stan12345", 
+        email: "Stan123@gmail.com", 
+        firstname: "dan", 
+        lastname: "ann", 
         phone: "1234567", 
         address: "1234567"
     }
  
     describe('findBy()', () => {
-        it('finds the user by the correct param', async () => {
+        it('finds the user by the correct parameter', async () => {
             await users.add(mockUser);
 
             let foundUser = await users.findBy({ username: mockUser.username });
             expect(foundUser.email).toEqual(mockUser.email);
         })
     });
+
     describe('add()', () => {
-        it('should add a new user', async () => {
+        it('should add a new user to the db', async () => {
             await users.add(mockUser);
             const user = await db('users');
             expect(user).toHaveLength(1);
         })
-        it('should add user and return created user with correct params', async () => {
+        it('should add user and return created user with correct parameters', async () => {
             const newUser = await users.add(mockUser);
             expect(newUser.username).toEqual(mockUser.username);
         })
     });
+
     describe('findById(id)', () => {
-        it('finds a user by id', async () => {
+        it('finds a user by its given id', async () => {
             const newUser = await users.add(mockUser);
             const foundUser = await users.findById(newUser.id);
             expect(foundUser.username).toEqual(newUser.username);
